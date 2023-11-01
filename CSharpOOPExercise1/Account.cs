d3c16271a8a806632651bd40ab67f8e22c648112
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-10-31 23:00:53 -0400

    Adicionar arquivos de projeto.

---
+++ CSharpOOPExercise1/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpOOPExercise1
+{
+    public class Account
+    {
+        public int AccountNumber { get; private set; }
+        public string Name { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(int accountNumber, string name)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+        }
+
+        public Account(int accountNumber, string name, double initialDepositAmount) : this(accountNumber, name)
+        {
+            Deposit(initialDepositAmount);
+        }
+
+        public void Withdraw(double n)
+        {
+            Balance -= n + 5;
+        }
+
+        public void Deposit(double n)
+        {
+            Balance += n;
+        }
+
+        public override string ToString() { return $"Conta {AccountNumber}, Titular: {Name}, Saldo {Balance}"; }
+
+    }
+}
---
+++ CSharpOOPExercise1/Program.cs
@@ -0,0 +1,41 @@
+﻿using CSharpOOPExercise1;
+using System;
+using System.Security.Principal;
+
+namespace CSharpOOPExecise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre o número da conta:");
+            int accountNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do titular da conta:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Você deseja adicionar um saldo inicial? (S para Sim, N para Não):");
+            string choice = Console.ReadLine();
+
+            Account account;
+
+            if (choice.ToUpper() == "S")
+            {
+                Console.WriteLine("Digite o saldo inicial:");
+                int initialDepositAmount = int.Parse(Console.ReadLine());
+
+                account = new Account(accountNumber, name, initialDepositAmount);
+            }
+            else
+            {
+                account = new Account(accountNumber, name);
+            }
+
+            Console.WriteLine("Digite um valor para saque:");
+            account.Withdraw(double.Parse(Console.ReadLine()));
+            Console.WriteLine(account.ToString());
+
+            Console.WriteLine("Digite um valor para deposito:");
+            account.Deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine(account.ToString());
+        }
+    }
+}
